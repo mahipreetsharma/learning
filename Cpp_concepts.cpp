44a85f58bdfd6385feec065ce88c384527622ee0
mahipreetsharma <sharmamahipreet@gmail.com>
2026-04-12 16:27:03 +0530

    first commit

--- Cpp_concepts.cpp
+++ Cpp_concepts.cpp
@@ -1,1 +1,15 @@
-//Arrays
+/*
+Arrays
+    Collection of data which are of same data type
+
+    when we declare a array the array whill be stored in stack in Main Memory
+    int a[5]; declaration of array
+    initialization of array
+    int a[5] = {1,2,3,4,5};
+
+    we use loops to scan or traverse through the array to access the element present in array
+    we can skip the size of the array while initializing
+
+    sizeof will provide the logical size of array arr[3] = {1,2,3} will have size of 21
+    
+*/  
