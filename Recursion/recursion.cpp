36a9ec5cbfc85361c48f701129ceed598a60b288
mahipreetsharma <sharmamahipreet@gmail.com>
2026-04-15 17:01:49 +0530

    Add documentation for recursion concepts and examples in recursion.txt

---
+++ Recursion/recursion.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+// void fun1(int n){ //print then recursion
+//     if(n>0){
+//         cout<<n;
+//         fun1(n-1);
+//     }
+// }
+
+// int main(){
+//     int x = 3;
+//     fun1(x);
+//     return 0;
+// }
+
+// void fun(int x){ recursion then print
+//     if(x>0){
+//         fun(x-1);
+//         cout<<x;
+//     }
+// }
+
+// int main(){
+//     int n = 5;
+//     fun(5);
+//     return 0;
+// }
