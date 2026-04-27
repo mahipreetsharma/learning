68c583ad4308a89fb00f1ee1f26c819912f4ec83
mahipreetsharma <sharmamahipreet@gmail.com>
2026-04-27 20:14:49 +0530

    adt

--- ADT/array/code.cpp
+++ ADT/array/code.cpp
@@ -2,18 +2,39 @@
 using namespace std;
 
 struct array{
-    int *a;
-    int size;
-    int length;
+    int *a;//array
+    int size;//size of array
+    int length;//length of array
 };
 
-int ar(array arr){
-    for(int i=0; i<arr.size; i++){
-        cout<<arr.a[i]<<endl;
+void Display(struct array arr){
+    int i;
+    cout<<"The elements are : "<<endl;
+    for(i=0; i<arr.length; i++){
+        cout<<arr.a[i];
     }
 }
 
 int main(){
     struct array arr;
-    arr.size=5;
+    cout<<"Enter the size of array : ";
+    cin>>arr.size;
+    
+    //creating array
+    arr.a = new int[arr.size];
+    arr.length=0;
+
+    int n , i;
+    cout<<"How many numbers :";
+    cin>>n;
+
+    cout<<"Enter all elements :";
+    for(int i = 0; i<n; i++){
+        cin>>arr.a[i];
+    }
+
+    arr.length=n;
+
+    Display(arr);
+    return 0;
 }
--- rough/index.cpp
+++ rough/index.cpp
@@ -2,10 +2,6 @@
 using namespace std;
 
 int main(){
-    int a[5] = {1,2,3,4,5};
-    int *p = a;
-    for(int i=0; i<5; i++){
-        cout<<p[i]<<" "<<&p[i]<<endl;
-    }
+    
     return 0;
 }
